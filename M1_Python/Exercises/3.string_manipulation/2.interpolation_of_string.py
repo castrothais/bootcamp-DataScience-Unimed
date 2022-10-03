1816dcd42a729ba47bc26f0911f8bcc410a79cec
Thaís Sampaio <thais_castrocs@outlook.com>
2022-10-03 13:05:21 -0300

    interpolation of strings

---
+++ M1_Python/Exercises/3.string_manipulation/2.interpolation_of_string.py
@@ -0,0 +1,16 @@
+name = "Thaís"
+age = 26
+course = "data science"
+language = "Python"
+
+print(
+    f"Hello. My name is {name} and I am {age} years old."
+    f"I am currently participating in the Unimed-BH {course}"
+    f"bootcamp inpartnership with DIO. The main language used is {language}."
+)
+
+
+# f-string
+PI = 3.141592
+
+print(f"pi value: {PI:.2f}")
