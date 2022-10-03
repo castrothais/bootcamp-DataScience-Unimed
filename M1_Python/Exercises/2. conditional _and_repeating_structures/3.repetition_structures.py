81ab1f5122981ca25028cc069749914656a24d96
Thaís Sampaio <thais_castrocs@outlook.com>
2022-10-03 12:26:04 -0300

    repeat structure example

---
+++ M1_Python/Exercises/2. conditional _and_repeating_structures/3.repetition_structures.py
@@ -0,0 +1,17 @@
+# Example 1:
+for n in range(3, 6):
+    print(n)
+# output: 3 4 5
+
+# Example 2:
+for n in range(0, 50, 5):
+    print(n, end=" ")
+# output: 0 5 10 15 20 25 30 35 40 45
+
+# Example 3:
+number = 1
+while number < 6:
+    print(number)
+    number += 1
+else:
+    print("number is no longer less than 6")
