f593c464a069e44a4c0e36356c646bd22f42b6c8
Thaís Sampaio <thais_castrocs@outlook.com>
2022-10-03 15:18:45 -0300

    strings with multiple lines

---
+++ M1_Python/Exercises/3.string_manipulation/4.strings_with_multiple_lines.py
@@ -0,0 +1,10 @@
+company = "Unimed-BH"
+
+print(
+    f"""The {company} Data Science bootcamp is for professionals who already
+work with data science or are starting their career. During the program,
+you will have educational experiences with Python, relational and
+non-relational databases, as well as an immersion in Cloud Computer and
+Machine Learning. After generating the certification of completion of the
+bootcamp, {company} intends to recruit talents to integrate its
+technology teams.""")
